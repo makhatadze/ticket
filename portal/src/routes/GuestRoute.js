80d21bb25318384d897f180104c07e49b5e8b4b7
makhatadze <vitomaxatadze@gmail.com>
2021-03-15 14:03:29 +0400

    Add routes and validation function.

---
+++ portal/src/routes/GuestRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const GuestRoute = ({ component: Component, auth, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            1 === false ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to="/dashboard" />
+            )
+        }
+    />
+);
+
+GuestRoute.propTypes = {
+    // auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    // auth: state.auth
+});
+
+export default connect(mapStateToProps)(GuestRoute);
---
+++ portal/src/routes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import GuestRoute from "./GuestRoute";
+
+
+const Routes = () => {
+    return (
+        <>
+            <Switch>
+                <Route exact path='/' component{} />
+            </Switch>
+        </>
+    )
+}
