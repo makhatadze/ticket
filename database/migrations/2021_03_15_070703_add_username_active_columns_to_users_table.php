95cd6dcc36fd865fccd6f4ea56f277eacfc4bba1
makhatadze <vitomaxatadze@gmail.com>
2021-03-15 11:09:17 +0400

    Add username and active columns to users table.

---
+++ database/migrations/2021_03_15_070703_add_username_active_columns_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  database/migrations/2021_03_15_070703_add_username_active_columns_to_users_table.php
+ *
+ * Date-Time: 15.03.21
+ * Time: 11:07
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsernameActiveColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique()->after('name');
+            $table->boolean('active')->default(true)->after('username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('active');
+        });
+    }
+}
