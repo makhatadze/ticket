4d4827166e15493352b1d6d541dc2b4886514d83
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 13:01:15 +0400

    Create role,dashboard routes intro layout group.

--- portal/src/routes/index.js
+++ portal/src/routes/index.js
@@ -4,16 +4,28 @@ import GuestRoute from "./GuestRoute";
 import Login from "../modules/Auth/Login";
 import {ToastContainer} from "react-toastify";
 import PrivateRoute from "./PrivateRoute";
-import Layout from "../modules/Layout/Layout";
+import CLayout from "../modules/Layout/Layout";
+import Dashboard from "../modules/Dashboard/Dashboard";
+import Role from "../modules/Role/Role";
 
 
 const Routes = () => {
     return (
         <>
-            <ToastContainer />
+            <ToastContainer/>
             <Switch>
-                <GuestRoute exact path='/login' component={Login} />
-                <PrivateRoute exact path='/dashboard' component={Layout} />
+                <Route exact path='/'>
+                    <Redirect to='/dashboard'/>
+                </Route>
+                <GuestRoute exact path='/login' component={Login}/>
+                <Route exact path='' render={() =>
+                    <CLayout>
+                        <Switch>
+                            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+                            <PrivateRoute exact path="/role" component={Role}/>
+                        </Switch>
+                    </CLayout>
+                }/>
             </Switch>
         </>
     )
