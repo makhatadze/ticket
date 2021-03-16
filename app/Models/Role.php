e276293a42991ebb06bda6cd0dfd78740f6b6b95
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 14:32:59 +0400

    Create role model with migration.

---
+++ app/Models/Role.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *  app/Models/Role.php
+ *
+ * Date-Time: 16.03.21
+ * Time: 14:24
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use RichanFongdasen\EloquentBlameable\BlameableTrait;
+
+/**
+ * Class Role
+ * @package App\Models
+ * @property integer $id
+ * @property string $name
+ * @property string $slug
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string|null $deleted_at
+ * @property integer $created_by
+ * @property integer $updated_by
+ * @property integer|null $deleted_by
+ *
+ * @mixin Builder
+ */
+class Role extends Model
+{
+    use BlameableTrait, softDeletes;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'roles';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+}
---
+++ database/migrations/2021_03_16_102254_create_roles_table.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *  database/migrations/2021_03_16_102254_create_roles_table.php
+ *
+ * Date-Time: 16.03.21
+ * Time: 14:32
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('created_by')->unsigned()->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('updated_by')->unsigned()->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('deleted_by')->unsigned()->nullable()->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
