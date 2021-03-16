c208371b3daae317d4bb690ab4df4500803dabaf
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 12:57:07 +0400

    Declare Role and Dashboard empty components.

---
+++ portal/src/modules/Role/Role.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Role extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Role
+            </div>
+        );
+    }
+}
+
+export default Role
