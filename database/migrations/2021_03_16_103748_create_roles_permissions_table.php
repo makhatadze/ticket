d73bd02b62ba719dda74f8bc07a4c3f71b3a6d0f
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 14:39:46 +0400

    Make migration roles_permissions table for connection roles,permissions columns.

---
+++ database/migrations/2021_03_16_103748_create_roles_permissions_table.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  database/migrations/2021_03_16_103748_create_roles_permissions_table.php
+ *
+ * Date-Time: 16.03.21
+ * Time: 14:37
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles_permissions', function (Blueprint $table) {
+            $table->bigInteger('role_id')->unsigned()->index();
+            $table->bigInteger('permission_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->primary(['role_id','permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles_permissions');
+    }
+}
