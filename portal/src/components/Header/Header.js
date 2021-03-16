1d1ac3352ad7af40138c4ae1b2874f0d52923595
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 12:55:32 +0400

    Create Header component for layout.

---
+++ portal/src/components/Header/Header.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import {Layout, Menu, Modal, Popover} from 'antd';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
+import PropTypes from "prop-types";
+import {logoutUser} from "../../actions/auth/authActions";
+import {sliderToggle} from "../../actions/layout/layoutActions";
+import "./Header.scss";
+import Avatar from "antd/es/avatar/avatar";
+
+const {Header} = Layout;
+const {Item} = Menu;
+
+class CHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            layout: {}
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.menuClick = this.menuClick.bind(this);
+
+    }
+
+    componentDidMount() {
+        this.setState({layout: this.props.layout});
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.layout) {
+            this.setState({layout: nextProps.layout});
+        }
+    }
+
+    toggle() {
+        this.props.sliderToggle(!this.state.layout.collapsed);
+    }
+
+    menuClick({key}) {
+        if (key === 'logout') {
+            this.props.logoutUser();
+        }
+    }
+
+
+    render() {
+        const {layout} = this.state;
+        const {currentProfile} = this.props.profile;
+        return (
+            <Header className="header">
+                {layout.collapsed ? (
+                        <MenuUnfoldOutlined className="trigger" onClick={this.toggle}/>
+                    )
+                    : (
+                        <MenuFoldOutlined className="trigger" onClick={this.toggle}/>
+                    )}
+                <Popover content={
+                    <Menu selectable={false} onClick={this.menuClick}>
+                        <Item key="logout">logout</Item>
+                    </Menu>
+                }
+                         placement="bottom"
+                         trigger="hover"
+                >
+                    <span className="user">hi，{currentProfile.user.name}</span>
+                    {/*<Avatar src={require('./../../assets/author.jpg')}></Avatar>*/}
+                </Popover>
+            </Header>
+        )
+    }
+}
+
+CHeader.propTypes = {
+    sliderToggle: PropTypes.func.isRequired,
+    logoutUser: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+    layout: state.layout,
+    profile: state.profile
+});
+
+
+export default connect(mapStateToProps, {sliderToggle, logoutUser})(withRouter(CHeader))
