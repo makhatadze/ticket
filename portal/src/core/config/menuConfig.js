c5f192088b53ad67f464a150b7427405761b28b4
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 12:57:54 +0400

    Import menu items from core/config/menuConfig.

---
+++ portal/src/core/config/menuConfig.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        title: 'Dashboard',
+        key: '/dashboard',
+        icon: 'HomeOutlined',
+        isLevel: true
+    },
+    {
+        title: 'Role',
+        key: '/role',
+        icon: 'LockOutlined',
+        isLevel: true
+
+    }
+]
