fa33e24ffc654049595aab3a9041238b34b6d332
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 15:14:56 +0400

    Make repository pattern and generate BaseRepository with EloquentRepositoryInterface.

---
+++ app/Repositories/Eloquent/Base/BaseRepository.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ *  app/Repositories/Eloquent/Base/BaseRepository.php
+ *
+ * Date-Time: 16.03.21
+ * Time: 14:52
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Repositories\Eloquent\Base;
+
+use App\Exceptions\ValidationException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+
+class BaseRepository implements EloquentRepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * Class Constructor
+     *
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Get all
+     *
+     * @param array $columns
+     *
+     * @return Collection
+     */
+    public function all($columns = ["*"]): Collection
+    {
+        return $this->model->get($columns);
+    }
+
+    /**
+     * Paginate all
+     *
+     * @param integer $perPage
+     * @param array $columns
+     *
+     * @return Paginator
+     */
+    public function paginate($perPage = 15, $columns = ['*']): Paginator
+    {
+        return $this->model->paginate($perPage, $columns);
+    }
+
+    /**
+     * Create new model
+     *
+     * @param array $attributes
+     *
+     * @return Model
+     */
+    public function create($attributes = []): Model
+    {
+        return $this->model->create($attributes);
+    }
+
+    /**
+     * Update model by the given ID
+     *
+     * @param integer $id
+     * @param array $data
+     * @param string $attribute
+     *
+     * @return mixed
+     */
+    public function update(int $id, $data = [], $attribute = 'id')
+    {
+        return $this->findOrFail($id)->update($data);
+    }
+
+    /**
+     * Delete model by the given ID
+     *
+     * @param integer $id
+     *
+     * @return boolean
+     */
+    public function delete(int $id): bool
+    {
+        return $this->model->destroy($id);
+    }
+
+    /**
+     * Restore model by the given ID
+     *
+     * @param integer $id
+     *
+     * @return boolean
+     */
+    public function restore(int $id): bool
+    {
+        return $this->findTrash($id)->restore();
+    }
+
+
+
+    /**
+     * Find model by the given ID
+     *
+     * @param integer $id
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function find(int $id, $columns = ['*'])
+    {
+        return $this->model->find($id, $columns);
+    }
+
+    /**
+     * Find model by a specific column
+     *
+     * @param string $field
+     * @param mixed $value
+     * @param array $columns
+     *
+     * @return mixed
+     */
+    public function findBy(string $field, $value, $columns = ['*'])
+    {
+        return $this->model->where($field, $value)->first($columns);
+    }
+
+    /**
+     * Find model by the given ID
+     *
+     * @param integer $id
+     * @param array $columns
+     *
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function findOrFail(int $id, $columns = ['*'])
+    {
+        $data = $this->model->find($id, $columns);
+        if (!$data) {
+            throw new ValidationException();
+        }
+        return $data;
+    }
+
+    /**
+     * Find model by the given ID
+     *
+     * @param integer $id
+     * @param array $columns
+     *
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function firstOrFail(int $id, $columns = ['*'])
+    {
+        $data = $this->model->firstOrFail($id, $columns);
+        if (!$data) {
+            throw new ValidationException();
+        }
+        return $data;
+    }
+
+    /**
+     * Restore model by the given ID
+     *
+     * @param integer $id
+     *
+     * @return Model
+     * @throws TrashException
+     */
+    public function findTrash(int $id): Model
+    {
+        $model = $this->model->withTrashed()->find($id);
+        if (null === $model->deleted_at) {
+            throw new TrashException();
+        }
+        return $model;
+    }
+}
---
+++ app/Repositories/Eloquent/Base/EloquentRepositoryInterface.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ *  app/Repositories/Eloquent/Base/EloquentRepositoryInterface.php
+ *
+ * Date-Time: 16.03.21
+ * Time: 14:52
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Repositories\Eloquent\Base;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+
+/**
+ * Interface EloquentRepositoryInterface
+ * @package App\Repositories\Eloquent\Base
+ */
+interface EloquentRepositoryInterface
+{
+
+    /**
+     * @param array $columns
+     *
+     * @return Collection
+     */
+    public function all(array $columns): ?Collection;
+
+    /**
+     * @param int $perPage
+     * @param array $columns
+     *
+     * @return Paginator
+     */
+    public function paginate(int $perPage, array $columns): ?Paginator;
+
+    /**
+     * @param array $attributes
+     *
+     * @return Model
+     */
+    public function create(array $attributes): Model;
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @param string $attribute
+     *
+     */
+    public function update(int $id, $data = [], $attribute = 'id');
+
+    /**
+     * @param integer $id
+     *
+     * @return boolean
+     */
+    public function delete(int $id): bool;
+
+    /**
+     * @param integer $id
+     *
+     * @return boolean
+     */
+    public function restore(int $id);
+
+    /**
+     * @param integer $id
+     * @param array $columns
+     */
+    public function find(int $id, $columns = ['*']);
+
+    /**
+     * @param string $field
+     * @param mixed $value
+     * @param string[] $columns
+     */
+    public function findBy(string $field, $value, $columns = ['*']);
+
+    /**
+     * @param integer $id
+     * @param array $columns
+     */
+    public function findOrFail(int $id, $columns = ['*']);
+
+    /**
+     * @param integer $id
+     *
+     * @return Model
+     */
+    public function findTrash(int $id): Model;
+}
