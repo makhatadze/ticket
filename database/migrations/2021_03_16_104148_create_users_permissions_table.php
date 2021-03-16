e52999ff1c2ece4214dc3c035a8965021a3c624d
makhatadze <vitomaxatadze@gmail.com>
2021-03-16 14:42:34 +0400

    Make migration users_permissions table for connection users,permissions columns.

--- database/migrations/2021_03_16_104148_create_users_permissions_table.php
+++ database/migrations/2021_03_16_104148_create_users_permissions_table.php
@@ -1,16 +1,16 @@
 <?php
 /**
- *  database/migrations/2021_03_16_104051_create_users_roles_table.php
+ *  database/migrations/2021_03_16_104148_create_users_permissions_table.php
  *
  * Date-Time: 16.03.21
- * Time: 14:41
+ * Time: 14:42
  * @author Vito Makhatadze <vitomaxatadze@gmail.com>
  */
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersRolesTable extends Migration
+class CreateUsersPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -19,12 +19,12 @@ class CreateUsersRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_roles', function (Blueprint $table) {
+        Schema::create('users_permissions', function (Blueprint $table) {
             $table->bigInteger('user_id')->unsigned()->index();
-            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('permission_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->primary(['user_id', 'role_id']);
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->primary(['user_id','permission_id']);
         });
     }
 
@@ -35,6 +35,6 @@ class CreateUsersRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_roles');
+        Schema::dropIfExists('users_permissions');
     }
 }
