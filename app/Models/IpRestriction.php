8d3573c7ecd44823606c17b1614241619745c518
makhatadze <vitomaxatadze@gmail.com>
2021-03-19 10:52:48 +0400

    Create ipRestrictions model and migration.

---
+++ app/Models/IpRestriction.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *  app/Models/IpRestriction.php
+ *
+ * Date-Time: 19.03.21
+ * Time: 10:48
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Models;
+
+use App\Traits\CustomBleableTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use RichanFongdasen\EloquentBlameable\BlameableTrait;
+
+/**
+ * Class IpRestriction
+ * @package App\Models
+ *
+ * @property integer $id
+ * @property string $ip
+ * @property boolean $status
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $deleted_at
+ * @property integer $created_by
+ * @property integer $updated_by
+ * @property integer $deleted_by
+ */
+class IpRestriction extends Model
+{
+    use softDeletes, BlameableTrait, CustomBleableTrait;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ip_restrictions';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ip',
+        'status'
+    ];
+}
---
+++ database/migrations/2021_03_19_064525_create_ip_restrictions_table.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *  database/migrations/2021_03_19_064525_create_ip_restrictions_table.php
+ *
+ * Date-Time: 19.03.21
+ * Time: 10:50
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIpRestrictionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ip_restrictions', function (Blueprint $table) {
+            $table->id();
+            $table->ipAddress('ip');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('created_by')->unsigned()->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->unsigned()->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('deleted_by')->unsigned()->nullable()->constrained('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ip_restrictions');
+    }
+}
