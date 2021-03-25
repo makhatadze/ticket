80bd608a796dc249ff02cb971a49be3caebeaa1a
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-03-25 20:16:19 +0400

    Add route user.

--- portal/src/routes/index.js
+++ portal/src/routes/index.js
@@ -10,6 +10,7 @@ import Role from "../modules/Role/Role";
 import IpRestriction from "../modules/IpRestriction/IpRestriction";
 import NotFound from "../modules/404/NotFound";
 import ChangePassword from "../modules/Profile/ChangePassword";
+import User from "../modules/User/User";
 
 
 const Routes = () => {
@@ -27,6 +28,7 @@ const Routes = () => {
                             <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                             <PrivateRoute exact path="/role" component={Role}/>
                             <PrivateRoute exact path="/ip-restriction" component={IpRestriction}/>
+                            <PrivateRoute exact path="/user" component={User}/>
                             <PrivateRoute exact path="/change-password" component={ChangePassword} />
                             <PrivateRoute component={NotFound} />
                         </Switch>
