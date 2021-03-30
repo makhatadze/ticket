3c781ce3e614f74d4a7e5af54e1dc07f8c16f4ec
makhatadze <vitomaxatadze@gmail.com>
2021-03-30 16:14:03 +0400

    Add ExportLogController.

---
+++ app/Http/Controllers/Api/v1/ExportLogController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  app/Http/Controllers/Api/v1/ExportLogController.php
+ *
+ * Date-Time: 30.03.21
+ * Time: 16:13
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ExportLogController extends Controller
+{
+    //
+}
