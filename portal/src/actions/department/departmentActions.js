65b5895752dc9d3c76b3cee11a5cc0280f6a89f6
makhatadze <vitomaxatadze@gmail.com>
2021-03-31 10:18:15 +0400

    getDepartmentsAction with GET_DEPARTMENTS mutation.

--- portal/src/actions/department/departmentActions.js
+++ portal/src/actions/department/departmentActions.js
@@ -1,8 +1,28 @@
 import axios from "axios";
-import {SET_DEPARTMENTS_LOADING} from "./departmentTypes";
+import {GET_DEPARTMENTS, SET_DEPARTMENTS_LOADING} from "./departmentTypes";
 
 const url = process.env.MIX_SERVER_API_URL;
 
+
+// Get Departments
+export const getDepartments = () => (dispatch, getState) => {
+    dispatch(setDepartmentsLoading());
+    const {searchQuery} = getState().departments;
+    axios
+        .get(`${url}/department${searchQuery}`)
+        .then(res =>
+            dispatch({
+                type: GET_DEPARTMENTS,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: GET_DEPARTMENTS,
+                payload: {}
+            })
+        )
+}
 
 // Set Departments Loading
 export const setDepartmentsLoading = () => {
