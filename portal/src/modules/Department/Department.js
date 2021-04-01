7dd593a45f4d4989f799c76818bfea6f234934ad
makhatadze <vitomaxatadze@gmail.com>
2021-04-01 09:52:50 +0400

    Paint departments table.

--- portal/src/modules/Department/Department.js
+++ portal/src/modules/Department/Department.js
@@ -1,12 +1,53 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {getDepartments} from "../../actions/department/departmentActions";
+import {Button, Table} from "antd";
 
-export default class Department extends Component{
+class Department extends Component {
     constructor(props) {
         super(props);
     }
+
+    componentDidMount() {
+        this.props.getDepartments();
+    }
+
     render() {
-        return(
-            <h1>Department</h1>
+        const {data, searchParams} = this.props.departments
+
+        return (
+            <div className="department">
+                <div className="row mb-4 action-container">
+                    <div className="col">
+                        <Button type="primary" onClick={() => console.log('show Form')}>Create Role</Button>
+                        <Button className="ml-2" type="primary"
+                                onClick={() => console.log('show filter')}>Filter</Button>
+                        <Button className="ml-2" type="primary"
+                                onClick={() => window.print()}>Print</Button>
+                    </div>
+                </div>
+                <Table
+                    columns={this.columns}
+                    rowKey={record => record.id}
+                    dataSource={data}
+                    pagination={searchParams}
+                    loading={searchParams.loading}
+                    onChange={this.handleTableChange}
+                />
+            </div>
         )
     }
 }
+
+Department.propTypes = {
+    getDepartments: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    departments: state.departments
+})
+
+export default connect(mapStateToProps, {
+    getDepartments
+})(Department)
