332f1821e829e5da6fb16a1084ee153cb69bcf2b
makhatadze <vitomaxatadze@gmail.com>
2021-04-01 09:57:46 +0400

    Add departmentReducer actions SET_DEPARTMENT_LOADING and GET_DEPARTMENTS.

--- portal/src/reducers/modules/departmentReducer.js
+++ portal/src/reducers/modules/departmentReducer.js
@@ -1,3 +1,5 @@
+import {GET_DEPARTMENTS, SET_DEPARTMENTS_LOADING} from "../../actions/department/departmentTypes";
+
 const initialState = {
     data: [],
     searchParams: {
@@ -20,6 +22,24 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
+        case SET_DEPARTMENTS_LOADING:
+            return {
+                ...state,
+                searchParams: {
+                    ...state.searchParams,
+                    loading: true
+                }
+            }
+        case GET_DEPARTMENTS:
+            return {
+                ...state,
+                data: action.payload.data,
+                searchParams: {
+                    ...state.searchParams,
+                    ...action.payload.pagination,
+                    loading: false
+                }
+            }
         default:
             return state;
     }
